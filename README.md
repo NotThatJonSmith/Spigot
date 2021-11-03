c0a1d0143686d379a0aecb27668a2cd35cfbcde8
Jonathan Smith <jondsmith22@gmail.com>
2021-11-03 08:52:45 -0500

    Add Spigot class and update README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Spigot
 A service thread and ring buffer geared for simplicity and low latency
+
+Let's address the obvious hiccup: why use a spinlock here? Isn't that inefficient?
+
+This thing is obsessed with consumer latency. If you're offloading too much work onto the producer thread and underflowing the buffer on the consumer end, you're hosed anyway, so spinning on consume isn't a problem and it's better to not have any locking overhead there.
+
+Spinning on the producer side isn't a problem because this project isn't meant for things that run on batteries ;)
---
+++ include/Spigot.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <thread>
+#include <condition_variable>
+
+template<typename Element, unsigned int size>
+class Spigot {
+
+private:
+
+    std::function<void(Element*)> Produce;
+    enum ThreadState { Paused, Running, Halted };
+    std::atomic<ThreadState> state;
+    std::mutex producerFunctionLock;
+    std::condition_variable stateChangedCondition;
+    std::thread producerThread;
+    Element buffer[size];
+    std::atomic<unsigned int> readIndex;
+    std::atomic<unsigned int> writeIndex;
+
+public:
+
+    inline Spigot() :
+        producerThread(&Spigot::ProducerThread, this),
+        readIndex(0),
+        writeIndex(0) {
+        Pause();
+    }
+
+    inline ~Spigot() {
+        Run();
+        state = Halted;
+        producerThread.join();
+    }
+
+    inline void SetProducer(std::function<void(Element*)> producerFunction) {
+        Produce = producerFunction;
+    }
+
+    inline void Run() {
+        if (state != Running) {
+            producerFunctionLock.unlock();
+            state = Running;
+            stateChangedCondition.notify_one();
+        }
+    }
+
+    inline void Pause() {
+        if (state != Paused) {
+            state = Paused;
+            producerFunctionLock.lock();
+            writeIndex = (readIndex + 1) % size;
+        }
+    }
+
+    inline Element* Current() {
+        while (readIndex == writeIndex);
+        return &buffer[readIndex];
+    }
+
+    inline Element* Next() {
+        readIndex = (readIndex + 1) % size;
+        return Current();
+    }
+
+private:
+
+    inline void ProducerThread() {
+
+        std::unique_lock<std::mutex> lk(producerFunctionLock);
+
+        while (true) {
+
+            while (state != Running) {
+                stateChangedCondition.wait(lk);
+                if (state == Halted) {
+                    return;
+                }
+            }
+
+            if (((writeIndex + 1) % size) != readIndex) {
+                Produce(&buffer[writeIndex]);
+                writeIndex = (writeIndex + 1) % size;
+                continue;
+            }
+
+        }
+    }
+};
